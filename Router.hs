a82a8186c8817a8259228dc7d0d22f1bfea8270c
Matt Parsons <parsonsmatt@gmail.com>
2015-05-28 17:45:00 -0400

    some instances, a bit more general

--- Router.hs
+++ Router.hs
@@ -2,15 +2,24 @@ module Router where
 
 import Data.Char
 
-data Action =
-    Show Integer 
-  | Edit Integer 
-  | Update Integer 
-  | Destroy Integer 
+data Action a =
+    Show a 
+  | Edit a 
+  | Update a 
+  | Destroy a 
   | Index 
   | New 
   | Create 
   deriving (Eq, Show)
+
+instance Functor Action where
+    fmap f (Show a)     = Show (f a)
+    fmap f (Edit a)     = Edit (f a)
+    fmap f (Update a)   = Update (f a)
+    fmap f (Destroy a)  = Destroy (f a)
+    fmap _ Index        = Index
+    fmap _ New          = New
+    fmap _ Create       = Create
 
 data Resources =
     Users
@@ -18,51 +27,54 @@ data Resources =
   | Comments
   deriving (Show, Eq)
 
-data Route =
-    Do Action Resources
-  | In Resources Integer Route
+data Route a =
+    Do (Action a) Resources
+  | In Resources a (Route a)
   deriving (Show, Eq)
 
+instance Functor Route where
+    fmap f (Do action res)  = Do (fmap f action) res
+    fmap f (In res x route) = In res (f x) (fmap f route)
 
-indexUsers :: Route
+indexUsers :: Route a
 indexUsers = Do Index Users
 
-inUser :: Integer -> Route -> Route
+inUser :: a -> Route a -> Route a
 inUser = In Users
 
-inPost :: Integer -> Route -> Route
+inPost :: a -> Route a -> Route a
 inPost = In Posts
 
 
-indexComments :: Route
+indexComments :: Num a => Route a 
 indexComments = In Users 3 $ In Posts 2 $ Do Index Comments
 
-createPost :: Route
+createPost :: Num a => Route a 
 createPost = In Users 2 $ Do Create Posts
 
-showUserPost :: Integer -> Integer -> Route
+showUserPost :: Num a => a -> a -> Route a
 showUserPost u n = In Users u $ Do (Show n) Posts
 
 
-toPath :: Route -> String
+toPath :: Show a => Route a -> String
 toPath =  map toLower . concat . rest
     where
         rest (Do action res) = ["/", show res, (getPathPiece . getIdentifier) action]
         rest (In res ident r) = ["/", show res, "/", show ident, toPath r]
 
 
-getAction :: Route -> Action
+getAction :: Route a -> Action a
 getAction (Do action _)  = action
 getAction (In _ _ route) = getAction route
 
 
-getIdentifier :: Action -> Maybe Integer
+getIdentifier :: Action a -> Maybe a
 getIdentifier (Show n)    = Just n
 getIdentifier (Edit n)    = Just n
 getIdentifier (Update n)  = Just n
 getIdentifier (Destroy n) = Just n
 getIdentifier _           = Nothing
 
-getPathPiece :: Maybe Integer -> String
+getPathPiece :: (Show a) => Maybe a -> String
 getPathPiece (Just n) = "/" ++ show n
 getPathPiece Nothing = ""
