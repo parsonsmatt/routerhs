9c6b3d26e854657eefe19c9ff36aede70b87df40
Matt Parsons <parsonsmatt@gmail.com>
2015-05-28 23:44:21 -0400

    trying a thing

--- Router2.hs
+++ Router2.hs
@@ -3,11 +3,17 @@
 module Router2 where
 
 data Action a where
-    Show :: Resource a -> Action a 
-    Edit :: Resource a -> Action a
-    Index :: Resources -> Action a
-    Create :: Resources -> Action a
+    Show   :: Resource a -> Action a 
+    Edit   :: Resource a -> Action a
+    Index  :: Resources  -> Action a
+    Create :: Resources  -> Action a
     deriving (Eq, Show)
+
+instance Functor Action where
+    fmap f (Show a)   = Show (fmap f a)
+    fmap f (Edit a)   = Edit (fmap f a)
+    fmap _ (Index a)  = Index a
+    fmap _ (Create a) = Create a
 
 data Resources =
     Users
@@ -17,9 +23,19 @@ data Resources =
 
 type Resource a = (Resources, a)
 
-data Route a =
-    Do (Action a)
-  | In (Resource a) (Route a)
+data Route =
+    Do Resources
+  | In Resources Route
   deriving (Show, Eq)
 
-infixl 3 `In`
+(<//>) :: Resources -> Route -> Route 
+(<//>) = In
+
+infixr 0 <//>
+
+routes :: [Route]
+routes =
+    [ Do Users
+    , Users <//> Do Posts
+    , Users <//> Posts <//> Do Comments
+    ]
